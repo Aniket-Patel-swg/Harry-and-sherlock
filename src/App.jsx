190ed8353f43e67851331676b1550f3f28f7aad5
Aniket-Patel-swg <patelaniket1207@gmail.com>
2023-12-19 11:02:30 +0530

    content changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,25 @@ function App() {
       <div className='main-div'>
           <h1>Investigate Harry Potter Scenario's</h1>
           <br />
-          <p>()</p>
+      </div>
+
+      <div className='situation'>
+        <h1>Situation: The Vanishing Broomstick</h1>
+        <p>
+          In the Gryffindor common room, during the events leading up to the 
+          Quidditch match between Gryffindor and Slytherin, Harry Potter's prized 
+          Firebolt broomstick suddenly goes missing. The room was locked, and only 
+          a few people had access.
+        </p>
+      </div>
+      <div className='information'>
+          <h2>Information:</h2>
+          <h3>1. Suspects:</h3>
+          <ul>
+            <li>Ron Weasley, who might be jealous and could have borrowed it without asking.</li>
+            <li>Draco Malfoy, Slytherin's Seeker, who may have stolen it to hinder Gryffindor's chances in the upcoming match.</li>
+            <li>  </li>
+          </ul>
       </div>
     </>
   )
