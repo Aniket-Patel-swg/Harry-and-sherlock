4b8a59d4c1774a1a56aed8018ec325d2b26c33b1
Aniket-Patel-swg <patelaniket1207@gmail.com>
2023-12-25 22:41:22 +0530

    Made audio changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import MovingCircle from './Components/MovingCircle'
 import {
@@ -9,8 +9,25 @@ import {
 } from "react-router-dom";
 import Letter from './Pages/Letter';
 import HomePage from './Pages/HomePage';
+import audioFile from './assets/hogwarts.mp3'
 
 function App() {
+
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    console.log('useEffect is running!')
+      console.log('play Audio Function is running!')
+      const audio = audioRef.current || new Audio(audioFile);
+      audioRef.current = audio;
+
+      audio.play().catch((error) => {
+        // Autoplay was prevented, request user interaction
+        if (error.name === 'NotAllowedError') {
+          console.log('Autoplay prevented. Please interact with the page and try again.');
+        }
+      });
+  }, []);
 
   return (
     <>
